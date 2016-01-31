4cbd933ad1051f8853aa1a66f6908da55aa352fe
Pablo Suárez Hernández <meaksh@gmail.com>
2016-01-31 15:40:34 +0000

    supervisord beacon: simple is better than complex

--- salt/beacons/supervisord.py
+++ salt/beacons/supervisord.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-Send events covering supervisord status
+Send events covering supervisord processes status
 '''
 
 # Import Python Libs
@@ -42,18 +42,25 @@ def beacon(config):
         beacons:
           supervisord:
             myapp1:
-              emitatstartup: False
-            myapp2:
               emitatstartup: True
-              onstatuschange: False
+            myapp2:
+              onchangeonly: True
               user:
               conf_file:
               bin_env:
 
-
     The config above sets up beacons to check for
     the myapp1 and myapp2 supervisord processes.
-    user, conf_file and bin_env are optionals and
+
+    `emitatstartup`: when `emitatstartup` is False the beacon will not fire
+    event when the minion is reload. Applicable only when `onchangeonly` is True.
+    The default is True.
+
+    `onchangeonly`: when `onchangeonly` is True the beacon will fire
+    events only when the process status changes.  Otherwise, it will fire an
+    event at each beacon interval.  The default is False.
+
+    'user', 'conf_file' and 'bin_env' are optionals and
     specify the supervisord enviroment.
     '''
     ret = []
@@ -65,49 +72,41 @@ def beacon(config):
         return ret
 
     for process in config:
-        process_status = __salt__['supervisord.status'](
-            process,
-            user=config[process].get('user'),
-            conf_file=config[process].get('conf_file'),
-            bin_env=config[process].get('bin_env'),
-        )
-
-        if not process in process_status:
-           log.info('supervisordprogram "{}" is not defined in ' \
-                    'configuration'.format(process))
-           continue
-
-        ret_dict = {}
-        ret_dict[process] = {'state': process_status[process]['state']}
-
         # default parameters
         _defaults = {
-            'emitatstartup': False,
-            'onstatuschange': True,
+            'emitatstartup': True,
+            'onchangeonly': False,
         }
 
         # If no parameters are provided, defaults are assigned
         if config[process] is None:
             config[process] = _defaults
 
-        # Including PID when supervisord program state is RUNNING
-        if ret_dict[process]['state'] == "RUNNING":
-            pid, status = process_status[process]['reason'].split(",")
-            ret_dict[process]['pid'] = pid
+        ret_dict = __salt__['supervisord.status'](
+            process,
+            user=config[process].get('user'),
+            conf_file=config[process].get('conf_file'),
+            bin_env=config[process].get('bin_env'),
+        )
 
-        # Emit the 'emitatstartup' event only if it's the first time
-        # for this process_uid
-        if process not in LAST_STATUS:
-            LAST_STATUS[process] = ret_dict[process].copy()
-            if config[process].get('emitatstartup', _defaults['emitatstartup']):
-                ret_dict[process]['emitatstartup'] = True
-                ret.append(ret_dict)
+        if not process in ret_dict:
+           log.info('supervisord program "{}" is not defined in ' \
+                    'configuration'.format(process))
+           continue
+
+        if config[process].get('onchangeonly', _defaults['onchangeonly']):
+            if process not in LAST_STATUS:
+                LAST_STATUS[process] = ret_dict[process]['state']
+                if config[process].get('emitatstartup', _defaults['emitatstartup']):
+                    ret_dict[process]['emitatstartup'] = True
+                    ret.append(ret_dict)
                 continue
 
-        if LAST_STATUS[process] != ret_dict[process]:
-            LAST_STATUS[process] = ret_dict[process].copy()
-            if config[process].get('onstatuschange', _defaults['onstatuschange']):
-                ret_dict[process]['onstatuschange'] = True
+            if LAST_STATUS[process] != ret_dict[process]['state']:
+                LAST_STATUS[process] = ret_dict[process]['state']
                 ret.append(ret_dict)
+
+        else:
+            ret.append(ret_dict)
 
     return ret
